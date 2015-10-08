3b430ccf6b70150775ebabe662eaf1d22b956f40
xyezir <xyezir@gmail.com>
2015-10-08 22:45:29 +0800

    rm .idea & add gitignore

--- resources/assets/js/admin/course/list.js
+++ resources/assets/js/admin/course/list.js
@@ -43,71 +43,85 @@ define(['jquery', 'dust', '$script'], function($, dust, $script) {
     $('#createCourseBtn').on('click', function(e) {
         var formId = 'createCourseForm';
         $('.ui.modals').remove();
-        dust.render(formTmpl, {
-            formId: formId,
-            header:'新增课程',
-            saveText: '下一步',
-            action: '/admin/courses',
-            method: 'POST',
-            stores: $.parseJSON($('#stores').val()),
-            coaches: $.parseJSON($('#coaches').val()),
-            modalId: 'createCourseModal'
-        }, function(err, result) {
-            document.body.insertAdjacentHTML('beforeend', result);
-            var classroomSelect = $('select[name="classroom_id"]');
 
-            $('select[name="store_id"]').dropdown({
-                action: function(text, value) {
-                    $.ajax({
-                        url: ['/admin/stores/', value, '/classrooms?is_select=1'].join(''),
-                        type: 'GET',
-                        dataType: 'json'
-                    }).done(function(ret){
-                        classroomSelect.empty();
-                        $.each(ret, function(index, classroom) {
-                            classroomSelect.append(['<option value="', classroom.id, '"', '>', classroom.name, '</option>'].join(''));
+        var createCourseModal = $('#createCourseModal'), createCourseCalendarModal = $('#createCourseCalendarModal');
+
+        if(createCourseModal.length) {
+            createCourseModal.modal('show');
+        } else {
+            dust.render(formTmpl, {
+                formId: formId,
+                header:'新增课程',
+                saveText: '下一步',
+                action: '/admin/courses',
+                method: 'POST',
+                stores: $.parseJSON($('#stores').val()),
+                coaches: $.parseJSON($('#coaches').val()),
+                modalId: 'createCourseModal'
+            }, function(err, result) {
+                document.body.insertAdjacentHTML('beforeend', result);
+                var classroomSelect = $('select[name="classroom_id"]');
+
+                $('select[name="store_id"]').dropdown({
+                    action: function(text, value) {
+                        $.ajax({
+                            url: ['/admin/stores/', value, '/classrooms?is_select=1'].join(''),
+                            type: 'GET',
+                            dataType: 'json'
+                        }).done(function(ret){
+                            classroomSelect.empty();
+                            $.each(ret, function(index, classroom) {
+                                classroomSelect.append(['<option value="', classroom.id, '"', '>', classroom.name, '</option>'].join(''));
+                            });
+                            classroomSelect.dropdown('refresh');
                         });
-                        classroomSelect.dropdown('refresh');
-                    });
-                    $('select[name="store_id"]').dropdown('set selected', value);
-                }
+                        $('select[name="store_id"]').dropdown('set selected', value);
+                    }
+                });
+
+                classroomSelect.dropdown();
+                $('select[name="coach_id"]').dropdown();
+
+                var $form = $('#' + formId);
+
+                formValid($form);
+
+                $('#createCourseModal').modal({
+                    closable  : false,
+                    allowMultiple: false,
+                    onDeny    : function(){
+                    },
+                    onApprove : function() {
+
+                        if(createCourseCalendarModal.length) {
+                            createCourseCalendarModal.modal('show');
+                        } else {
+                            dust.render(calendarTmpl, {
+                                header:'设置课程时间',
+                                saveText: '保 存',
+                                backText: '上一步',
+                                modalId: 'createCourseCalendarModal'
+                            }, function(err, result) {
+                                document.body.insertAdjacentHTML('beforeend', result);
+                                $('#createCourseCalendarModal').modal({
+                                    closeable: false,
+                                    allowMultiple: false,
+                                    onDeny: function() {
+                                        createCourseModal.modal('show');
+                                    },
+                                    onApprove : function() {
+
+                                    }
+                                }).modal('show');
+                            });
+                        }
+                        return false;
+                    }
+                }).modal('show');
             });
+        }
 
-            classroomSelect.dropdown();
-            $('select[name="coach_id"]').dropdown();
 
-            var $form = $('#' + formId);
-
-            formValid($form);
-
-            $('#createCourseModal').modal({
-                closable  : false,
-                allowMultiple: false,
-                onDeny    : function(){
-                },
-                onApprove : function() {
-                    dust.render(calendarTmpl, {
-                        header:'设置课程时间',
-                        saveText: '保 存',
-                        backText: '上一步',
-                        modalId: 'createCourseCalendarModal'
-                    }, function(err, result) {
-                        document.body.insertAdjacentHTML('beforeend', result);
-                        $('#createCourseCalendarModal').modal({
-                            closeable: false,
-                            allowMultiple: false,
-                            onDeny: function() {
-
-                            },
-                            onApprove : function() {
-
-                            }
-                        }).modal('show');
-                    });
-                    return false;
-                }
-            }).modal('show');
-        });
     });
 
     $('div[data-id="deleteCourseBtn"]').on('click', function(e) {
