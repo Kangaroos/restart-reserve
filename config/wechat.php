e751e842b81cb136a6b5f41f9122c9e8454657c6
xyezir <xyezir@gmail.com>
2015-10-26 12:36:38 +0800

    修改微信配置到.env文件

--- config/wechat.php
+++ config/wechat.php
@@ -1,8 +1,8 @@
 <?php
 return [
-    'use_alias'    => false,
-    'app_id'       => 'wx746dcd36de9fd58f', // 必填
-    'secret'       => 'd2165c6a85253f43aead3c79505fc740', // 必填
-    'token'        => 'JFKSU934H8FU9FH9AS',  // 必填
-    'encoding_key' => 'YourEncodingAESKey' // 加密模式需要，其它模式不需要
+    'use_alias'    => env('WX_USE_ALIAS', false),
+    'app_id'       => env('WX_APP_ID', 'YourAppID'), // 必填
+    'secret'       => env('WX_SECRET', 'YourSecret'), // 必填
+    'token'        => env('WX_TOKEN', 'YourToken'),  // 必填
+    'encoding_key' => env('WX_ENCODING_KEY','YourEncodingAESKey') // 加密模式需要，其它模式不需要
 ];
